b7f28e6a83e35a93d6351ebcfa2c422486a25537
AbhiramRuthala <134224393+AbhiramRuthala@users.noreply.github.com>
2023-07-05 16:57:44 -0400

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,24 @@
+num = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+userinput2 = input("What operation do you want to do?: ")
+
+if userinput2 == "+" : 
+  result = num+num2
+
+elif userinput2 == "-" : 
+  result = num-num2
+
+elif userinput2 == "*" : 
+  result = num*num2
+
+elif userinput2 == "/" : 
+  result = num/num2
+
+print("Your answer is:", result)
+
+if result==result: {
+  print("Correct!")
+  
+}
+
